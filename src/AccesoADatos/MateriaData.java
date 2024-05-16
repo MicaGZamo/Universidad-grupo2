2ecf39a11fb83130c423ea01dad8b12be204b3d3
Ing. Ramiro Echenique <117690034+RamiroEchenique@users.noreply.github.com>
2024-05-16 17:41:53 -0300

    clase MateriaDatos

---
+++ src/AccesoADatos/MateriaData.java
@@ -0,0 +1,15 @@
+
+package AccesoADatos;
+
+import java.sql.Connection;
+
+public class MateriaData {
+    
+    private Connection con = null;
+
+    public MateriaData() {
+    }
+  
+   
+    
+}
