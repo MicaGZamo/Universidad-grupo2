b11fae674b1c5568efffdc9773b0aca98e9edd6f
NadiaCcod <163307149+NadiaCcod@users.noreply.github.com>
2024-05-17 11:18:36 -0300

    Materia Data y prueba Main

--- src/AccesoADatos/MateriaData.java
+++ src/AccesoADatos/MateriaData.java
@@ -1,15 +1,43 @@
 
 package AccesoADatos;
-
+import java.sql.*;
+import Entidades.Materia;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 
 public class MateriaData {
     
     private Connection con = null;
 
     public MateriaData() {
+          con = Conexion.getConexion();
     }
   
-   
+   public void guardarMateria(Materia materia){
+      String sql = "INSERT INTO `materia`(`nombre`, `año`, `estado`) VALUES (?,?,?)";
+      
+      try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS); 
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getAnio());
+            ps.setBoolean(3, materia.isEstado());
+            ps.executeUpdate();
+            
+            ResultSet rs = ps.getGeneratedKeys();
+            
+            if(rs.next()){
+                materia.setIdMateria(rs.getInt("idMateria"));
+                JOptionPane.showMessageDialog(null, "Materia cargada correctamente");
+            }
+            ps.close();
+      } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla alumnos ");
+            System.out.println("error " + ex.getMessage());
+            ex.printStackTrace();
+        }
+            
+   }
     
 }
--- src/universidad/grupo2/UniversidadGrupo2.java
+++ src/universidad/grupo2/UniversidadGrupo2.java
@@ -3,7 +3,9 @@ package universidad.grupo2;
 //import org.mariadb.jdbc.Connection;
 import AccesoADatos.AlumnoData;
 import AccesoADatos.Conexion;
+import AccesoADatos.MateriaData;
 import Entidades.Alumno;
+import Entidades.Materia;
 import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.*;
@@ -17,35 +19,47 @@ public class UniversidadGrupo2 {
 
     public static void main(String[] args) {
         Connection con = Conexion.getConexion();
-        LocalDate fecha = LocalDate.of(1997, 05, 10);
-        Alumno alum1 = new Alumno("Silvera", 4781236, "Jose", fecha, true);
-        LocalDate fecha2 = LocalDate.of(1999, 05, 12);
-        Alumno alum2 = new Alumno("Soza", 4981236, "Julia", fecha2, true);
-        LocalDate fecha3 = LocalDate.of(1991, 12, 10);
-        Alumno alum3 = new Alumno("Fuentes", 39258123, "Gisela", fecha3, true);
+//        LocalDate fecha = LocalDate.of(1997, 05, 10);
+//        Alumno alum1 = new Alumno("Silvera", 4781236, "Jose", fecha, true);
+//        LocalDate fecha2 = LocalDate.of(1999, 05, 12);
+//        Alumno alum2 = new Alumno("Soza", 4981236, "Julia", fecha2, true);
+//        LocalDate fecha3 = LocalDate.of(1991, 12, 10);
+//        Alumno alum3 = new Alumno("Fuentes", 39258123, "Gisela", fecha3, true);
+//        
+//        
+//        AlumnoData ad = new AlumnoData();
+//        ad.guardarAlumno(alum1);
+//        ad.guardarAlumno(alum2);
+//        ad.guardarAlumno(alum3);
+//
+//        System.out.println(ad.buscarAlumno(2));
+//        System.out.println(ad.buscarAlumnoPorDni(39258123));
+//        
+//        List <Alumno> listadoA= ad.listarAlumnos();
+//        System.out.println("--- LISTADO ALUMNOS ---");
+//        for (Alumno alumno : listadoA) {
+//            System.out.println(alumno);
+//         
+//        }
+     // Alumno alum4 = new Alumno(4,"Sanchez", 35874123, "Sofia", fecha3, true); 
+     // ad.actualizarAlumno(alum4);
         
-        
-        AlumnoData ad = new AlumnoData();
-        ad.guardarAlumno(alum1);
-        ad.guardarAlumno(alum2);
-        ad.guardarAlumno(alum3);
-
-        System.out.println(ad.buscarAlumno(2));
-        System.out.println(ad.buscarAlumnoPorDni(39258123));
-        
-        List <Alumno> listadoA= ad.listarAlumnos();
-        System.out.println("--- LISTADO ALUMNOS ---");
-        for (Alumno alumno : listadoA) {
-            System.out.println(alumno);
-         
-        }
-      Alumno alum4 = new Alumno(4,"Sanchez", 35874123, "Sofia", fecha3, true); 
-      ad.actualizarAlumno(alum4);
-        
-        ad.darBaja(1);
+//        ad.darBaja(1);
       
+        //Creacion objetos Materias
+        
+        Materia mat1= new Materia("Matematica", 1, true);
+        Materia mat2= new Materia("Literatura", 1, true);
+        Materia mat3= new Materia("Quimica", 2, true);
+        //Agregar esas materias a la tabla
+        
+        MateriaData md = new MateriaData();
+        
+        md.guardarMateria(mat1);
+        md.guardarMateria(mat2);
+        md.guardarMateria(mat3);
     }
     
+} 
     
-    
-}
+
