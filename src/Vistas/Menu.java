159dd8c5e59798fb7cba960a8607c0c094ed6553
Micaela.G.Zamo <micazamo1@gmail.com>
2024-05-23 15:12:06 +0200

    Formulario Materia

--- src/Vistas/Menu.java
+++ src/Vistas/Menu.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package Vistas;
 
-/**
- *
- * @author gomez
- */
+
 public class Menu extends javax.swing.JFrame {
 
     /**
